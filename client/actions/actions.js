a88dd10e7f029588f9d61d0d81360557402cbde8
jaelee213 <jaelee213@gmail.com>
2019-07-25 16:08:09 -0700

    Add rxjs logic to avoid receiving stale search data

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -8,30 +8,51 @@
  */
 
 import * as types from '../constants/actionTypes';
+import { Observable } from 'rxjs';
 
-export const getBooks = title => dispatch => {
+export const getBooks = (title, prevSubs) => dispatch => {
   // no need to fetch if title is empty string
   if (!title.length) dispatch(fetchSuccess([]));
   else {
-    // action to fire up loading splash screen while fetching to api
-    dispatch(fetchInProgress());
-    const queryString = title.split(' ').join('+');
-    return fetch(`http://openlibrary.org/search.json?title=${queryString}`)
-    .then(res => res.json()) // convert response to json
-    .then(({ docs }) => {
-      // filter result by books that have title, isbn #, first publish year
-      const searchResults = docs.filter(book => book.title && book.isbn && book.first_publish_year);
-      dispatch(fetchSuccess(searchResults));
-    })
-    .catch(err => {
-      // fire the error handler action
-      dispatch(fetchError(err));
+    // if previous fetch subscription exists, unsubscribe from it
+    if (prevSubs !== null) {
+      prevSubs.unsubscribe();
+    }
+    // wrap the fetch process in observable so each fetch request can 
+    // easily be subscribed / unsubscribed to --> avoid stale data
+    const fetchData$ = Observable.create((observer) => {
+      const queryString = title.split(' ').join('+');
+      fetch(`http://openlibrary.org/search.json?title=${queryString}`)
+      .then(res => res.json()) // convert response to json
+      .then(({ docs }) => {
+        observer.next(docs); // emit the docs that was returned from api
+      })
+      .catch(err => {
+        observer.error(err); // emit upon encountered error (stops process)
+      });
     });
+    // subscribe to fetchData$ to start fetch process
+    const subscription = fetchData$.subscribe(
+      // first param is callback that listens to values emitted from observer.next
+      (docs) => {
+        // filter result by books that have title, isbn #, first publish year
+        const searchResults = docs.filter(book => book.title && book.isbn && book.first_publish_year);
+        dispatch(fetchSuccess(searchResults));
+      },
+      // second param is callback that listens to values emitted from observer.error
+      (err) => dispatch(fetchError(err)),
+      // third param is callback that listens to the invocation of observer.complete
+      null,
+    );
+     // dispatch action to indicate existing subscription (fetch in progress)
+     dispatch(fetchInProgress(subscription));
+    return;
   }
 };
   
-export const fetchInProgress = () => ({
+export const fetchInProgress = (subscription) => ({
   type: types.FETCH_IN_PROGRESS,
+  payload: subscription,
 });
 
 export const fetchSuccess = searchResults => ({
